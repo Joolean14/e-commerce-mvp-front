6a044287a56117942004d8cca2ce39479eefe63d
julian <Joolean14>
2022-01-15 15:18:15 -0500

    [Feature] Basic footer, logo and footer.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import {
   AiOutlineInstagram,
   AiOutlineYoutube,
 } from "react-icons/ai";
+
+import logo from "./falso-idolo-logo.png";
+
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <img className="logo" src={logo}></img>
+        <div className="menu">
+        </div>
+      </header>
       <footer className="footer">
         <div className="social-icons-container">
-          <AiFillFacebook></AiFillFacebook>
-          <AiOutlineInstagram></AiOutlineInstagram>
-          <AiOutlineYoutube></AiOutlineYoutube>
+          <AiFillFacebook className="footer-icon"></AiFillFacebook>
+          <AiOutlineInstagram className="footer-icon"></AiOutlineInstagram>
+          <AiOutlineYoutube className="footer-icon"></AiOutlineYoutube>
         </div>
-        <div>
-          <p>Falso Ídolo © | Terms & Conditions | Privacy Policy</p>
+        <div className="legal-container">
+          <p className="brand-name legal-text">Falso Ídolo ©</p>
+          <p className="legal-text">|</p>
+          <a className="legal-text">Terms & Conditions</a>
+          <p className="legal-text">|</p>
+          <a className="legal-text">Privacy Policy</a>
         </div>
       </footer>
     </div>
