155759b2dec05b6d9372674cb3e3b8e6d74063ca
julian <Joolean14>
2022-01-16 15:25:26 -0500

    [Feature] Add basic home html elements

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {
   AiFillFacebook,
   AiOutlineInstagram,
   AiOutlineYoutube,
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
 } from "react-icons/ai";
 
 import logo from "./falso-idolo-logo.png";
@@ -12,9 +15,78 @@ function App() {
     <div className="App">
       <header className="header">
         <img className="logo" src={logo} alt="Falso Ídolo Logo"></img>
-        <div className="menu">
-        </div>
+        <div className="menu"></div>
       </header>
+      <div className="filters">
+        <div className="filter-icons">
+          <AiOutlineSearch className="filter-icon"></AiOutlineSearch>
+          <AiOutlineShoppingCart className="filter-icon"></AiOutlineShoppingCart>
+          <AiOutlineUser className="filter-icon"></AiOutlineUser>
+        </div>
+        <div className="filter-categories">
+          <p>LIMITED COLLECTION</p>
+          <p>SHIRTS</p>
+          <p>HOODIES</p>
+          <p>ATHLEISURE</p>
+          <p>KIDS</p>
+          <p>MUSIC</p>
+          <p>POSTERS</p>
+          <p>BITS & BOBS</p>
+          <p>WAREHOUSE</p>
+        </div>
+      </div>
+      <div className="products-container">
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+        <div className="product-card">
+          <img src="https://picsum.photos/200"></img>
+          <p>Texas Moon CD</p>
+          <p>$12.00</p>         
+        </div>
+      </div>
       <footer className="footer">
         <div className="social-icons-container">
           <AiFillFacebook className="footer-icon"></AiFillFacebook>
